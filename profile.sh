172c8c2474f8c8286649630f332bbe85625033b3
Camp <12837401+campfred@users.noreply.github.com>
2025-12-10 14:12:00 -0500

    Bleh

--- profile.sh
+++ profile.sh
@@ -1,14 +1,25 @@
 #!/bin/env sh
 
+local shell_name="${SHELL##*/}"
+
 # Base shell configuration
 export HISTCONTROL=ignoreboth:erasedups
 export EDITOR="code --wait"
 export VISUAL="code --wait"
-# export BASE_SHELL=${SHELL##*/}
+
+# Autocompletion setup
+source "$(dirname $0)/configure-completion.sh" > /dev/null 2>&1
         
 # Homebrew setup
+# https://docs.brew.sh/Manpage#environment
 export HOMEBREW_BUNDLE_FILE="$(dirname $0)/.homebrew/brewfile"
 export HOMEBREW_NO_ANALYTICS=true
+export HOMEBREW_NO_ENV_HINTS=true
+
+# TFSwitch setup
+# https://tfswitch.warrensbox.com/usage/environment-variables/#tf_binary_path
+export TF_BINARY_PATH="${HOMEBREW_PREFIX:-/home/linuxbrew/.linuxbrew}/bin/terraform"
+export TF_PRODUCT=opentofu
 
 # # Pyenv setup
 # # https://github.com/pyenv/pyenv
