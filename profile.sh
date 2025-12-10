18439e58728bf532d6091a5c01bb1983e1e5e09a
Camp <12837401+campfred@users.noreply.github.com>
2025-12-10 10:37:32 -0500

    Rendre script de profil POSIX-compliant pour vrai

--- profile.sh
+++ profile.sh
@@ -1,14 +1,19 @@
-# # Python version management with pyenv
-# # https://github.com/pyenv/pyenv
-# # curl -fsSL https://pyenv.run | bash
-# export PYENV_ROOT="$HOME/.pyenv"
-# [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-# eval "$(pyenv init - $(basename $SHELL))"
-# eval "$(pyenv virtualenv-init -)"
+#!/bin/env sh
 
 # Base shell configuration
 export HISTCONTROL=ignoreboth:erasedups
 export EDITOR="code --wait"
 export VISUAL="code --wait"
-export HOMEBREW_BUNDLE_FILE="$HOME/.homebrew/brewfile"
-export BASE_SHELL=$(basename $SHELL)
+# export BASE_SHELL=${SHELL##*/}
+        
+# Homebrew setup
+export HOMEBREW_BUNDLE_FILE="$(dirname $0)/.homebrew/brewfile"
+export HOMEBREW_NO_ANALYTICS=true
+
+# # Pyenv setup
+# # https://github.com/pyenv/pyenv
+# # curl -fsSL https://pyenv.run | bash
+# export PYENV_ROOT="$HOME/.pyenv"
+# [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init - ${SHELL##*/})"
+# eval "$(pyenv virtualenv-init -)"
